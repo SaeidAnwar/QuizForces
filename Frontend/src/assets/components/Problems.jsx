d842a2037c3280aaa5b8088d31140a9b585c948a
saeidanwar <mdsaeid753@gmail.com>
2024-09-23 21:39:58 +0530

    problems are showing

--- Frontend/src/assets/components/Problems.jsx
+++ Frontend/src/assets/components/Problems.jsx
@@ -1,16 +1,136 @@
-import "./Problems.css"
+import "./Problems.css";
+
+const testData = [
+    {
+        sno: 1,
+        name: "first question",
+        difficulty: "easy",
+        solved: "no",
+    },
+    {
+        sno: 1,
+        name: "first question",
+        difficulty: "easy",
+        solved: "no",
+    },
+    {
+        sno: 1,
+        name: "first question",
+        difficulty: "easy",
+        solved: "no",
+    },
+    {
+        sno: 1,
+        name: "first question",
+        difficulty: "easy",
+        solved: "no",
+    },
+    {
+        sno: 1,
+        name: "first question",
+        difficulty: "easy",
+        solved: "no",
+    },
+    {
+        sno: 1,
+        name: "first question",
+        difficulty: "easy",
+        solved: "no",
+    },
+    {
+        sno: 1,
+        name: "first question",
+        difficulty: "easy",
+        solved: "no",
+    },
+    {
+        sno: 1,
+        name: "first question",
+        difficulty: "easy",
+        solved: "no",
+    },
+    {
+        sno: 1,
+        name: "first question",
+        difficulty: "easy",
+        solved: "no",
+    },
+    {
+        sno: 1,
+        name: "first question",
+        difficulty: "easy",
+        solved: "no",
+    },
+    {
+        sno: 1,
+        name: "first question",
+        difficulty: "easy",
+        solved: "no",
+    },
+    {
+        sno: 1,
+        name: "first question",
+        difficulty: "easy",
+        solved: "no",
+    },
+    {
+        sno: 1,
+        name: "first question",
+        difficulty: "easy",
+        solved: "no",
+    },
+    {
+        sno: 1,
+        name: "first question",
+        difficulty: "easy",
+        solved: "no",
+    },
+    {
+        sno: 1,
+        name: "first question",
+        difficulty: "easy",
+        solved: "no",
+    },
+];
 
 function Problems() {
     return (
         <div className="problems-cont">
+            <h3>Problems</h3>
             <table>
                 <tbody>
-                    <tr>
-                        <td className="sno">#</td>
-                        <td className="name">Name</td>
-                        <td className="diff">Difficulty</td>
-                        <td className="solved">Solved</td>
+                    <tr className="header">
+                        <td className="sno">
+                            <p>#</p>
+                        </td>
+                        <td className="name">
+                            <p>Name</p>
+                        </td>
+                        <td className="diff">
+                            <p>Difficulty</p>
+                        </td>
+                        <td className="solved">
+                            <p>Solved</p>
+                        </td>
                     </tr>
+                    {testData.map((el) => {
+                        return (
+                            <tr key={el.sno}>
+                                <td>
+                                    <p>{el.sno}</p>
+                                </td>
+                                <td>
+                                    <p>{el.name}</p>
+                                </td>
+                                <td>
+                                    <p>{el.difficulty}</p>
+                                </td>
+                                <td>
+                                    <p>{el.solved}</p>
+                                </td>
+                            </tr>
+                        );
+                    })}
                 </tbody>
             </table>
         </div>
