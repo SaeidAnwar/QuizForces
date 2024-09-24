7d9b2786667268fc4a7b2be51bef34ade45d9db3
saeidanwar <mdsaeid753@gmail.com>
2024-09-24 21:02:00 +0530

    added footer

---
+++ Frontend/src/assets/components/Footer.jsx
@@ -0,0 +1,12 @@
+import "./Footer.css"
+
+function Footer() {
+    return (
+        <div className="footer-cont">
+            <div className="separator"></div>
+            <p>Quizforces (c) Copyright 2024-2025 Md Saeid Anwar</p>
+        </div>
+    );
+}
+
+export default Footer;
